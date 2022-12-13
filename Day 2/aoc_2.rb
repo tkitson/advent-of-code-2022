3c6ce74de3d8f3263a5c9c39c2cf5de1c52a918d
Tom Kitson <thomaspfkitson@gmail.com>
2022-12-13 23:13:53 +0100

    day 2 pt 2

--- Day 2/aoc_2.rb
+++ Day 2/aoc_2.rb
@@ -1,26 +1,48 @@
 file = File.open("input.txt")
 pairs_arr = file.readlines.map(&:chomp)
 
-score = 0
-
 sub_arrays = pairs_arr.map { |elem| elem.split }
 
-sub_arrays.each do |sub|
-  if sub[1] == "X"
-    score += 1
-  elsif sub[1] == "Y"
-    score += 2
-  elsif sub[1] == "Z"
-    score += 3
-  end
-end
+#  P1
+
+# score = 0
+
+# sub_arrays.each do |sub|
+#   if sub[1] == "X"
+#     score += 1
+#   elsif sub[1] == "Y"
+#     score += 2
+#   elsif sub[1] == "Z"
+#     score += 3
+#   end
+# end
+
+# pairs_arr.each do |pair|
+#   if pair == "A X" || pair == "B Y" || pair == "C Z"
+#     score += 3
+#   elsif pair == "A Y" || pair == "B Z" || pair == "C X"
+#     score += 6
+#   end
+# end
+
+# P2
+
+score = 0
+
+p2_hash = {
+  "A X": 3,
+  "B X": 1,
+  "C X": 2,
+  "A Y": 4,
+  "B Y": 5,
+  "C Y": 6,
+  "A Z": 8,
+  "B Z": 9,
+  "C Z": 7,
+}
 
 pairs_arr.each do |pair|
-  if pair == "A X" || pair == "B Y" || pair == "C Z"
-    score += 3
-  elsif pair == "A Y" || pair == "B Z" || pair == "C X"
-    score += 6
+  score += p2_hash[pair.to_sym]
   end
-end
 
 p score
