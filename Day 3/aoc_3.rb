8f6aa92b25120bd6482dba3d3418b4ae628be920
Tom Kitson <thomaspfkitson@gmail.com>
2022-12-19 12:55:01 +0000

    day 3 part 2

--- Day 3/aoc_3.rb
+++ Day 3/aoc_3.rb
@@ -1,6 +1,8 @@
 file = File.open("input.txt")
 arr = file.readlines.map(&:chomp)
 
+# pt 1
+
 total = 0
 
 letters = ('a'..'z').to_a | ('A'..'Z').to_a
@@ -9,16 +11,27 @@ letters.each_with_index do |letter, index|
   alphabet_hash[letter] = index + 1
 end
 
-halves = []
+# halves = []
 
-arr.each do |bag|
- halves << bag.chars.each_slice(bag.size / 2).map(&:join)
-end
+# arr.each do |bag|
+#  halves << bag.chars.each_slice(bag.size / 2).map(&:join)
+# end
 
-halves.each do |half|
-  common_chars = half[0].chars & half[1].chars
+# halves.each do |half|
+#   common_chars = half[0].chars & half[1].chars
+#   common_chars.each do |char|
+#   total += alphabet_hash[char[0]]
+#   end
+# end
+# p total
+
+# pt 2
+
+
+arr.each_slice(3) do |a|
+  common_chars = a[0].chars & a[1].chars & a[2].chars
   common_chars.each do |char|
-  total += alphabet_hash[char[0]]
+    total += alphabet_hash[char[0]]
   end
+  p total
 end
-p total
